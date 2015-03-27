b4124044cc6f323e3f3b2c1935003f2dcbeb9b31
moisesjbc <moisesjbc@gmail.com>
2015-03-27 11:27:59 +0000

    Added snake's head to game.

--- snake.js
+++ snake.js
@@ -1,4 +1,15 @@
+var stage;
+var CELL_SIZE = 20;
+
 function init()
 {
-	alert( 'Snake!' );
+	stage = new createjs.Stage( "snakeCanvas" );
+
+	var snakeHead = new createjs.Shape();
+	snakeHead.graphics.beginFill("black").drawRect( 0, 0, CELL_SIZE, CELL_SIZE );
+	snakeHead.x = 0;
+	snakeHead.y = 0;
+	stage.addChild( snakeHead );
+
+	stage.update();
 }
