2790e78491637ad35e3092a6dbcabbcb45a41319
moisesjbc <moisesjbc@gmail.com>
2015-03-27 16:28:35 +0000

    Snake's logic has been moved to a new class Snake.

--- js/snake.js
+++ js/snake.js
@@ -1,23 +1,58 @@
 var stage;
 var CELL_SIZE = 20;
-var snake = [];
-var direction = new Direction( 'right' );
 var canvasWidth, canvasHeight;
 var snakeSize = 3;
-var snakeTailIndex;
-var snakeHeadIndex;
+var snake;
+
+function Snake(){
+	this.body = [];
+	this.direction = new Direction( 'right' );
+
+	for( i = 0; i < snakeSize; i++ ){
+		this.body[i] = new createjs.Shape();
+		this.body[i].graphics.beginFill("black");
+		this.body[i].graphics.drawRect( 0, 0, CELL_SIZE, CELL_SIZE );
+		stage.addChild( this.body[i] );
+	}
+	
+	this.reset = function(){
+		this.tailIndex = 0;
+		this.headIndex = snakeSize - 1;
+		for( i = 0; i < snakeSize; i++ ){
+			this.body[i].x = i * CELL_SIZE;
+			this.body[i].y = 0;
+			this.direction.set( 'right' );
+		}
+	}
+
+	
+	this.update = function( stage ){
+		this.body[this.tailIndex].x = this.body[this.headIndex].x + this.direction.x * CELL_SIZE;
+		this.body[this.tailIndex].y = this.body[this.headIndex].y + this.direction.y * CELL_SIZE;
+
+		this.headIndex = ( this.headIndex + 1 ) % snakeSize;
+		this.tailIndex = ( this.tailIndex + 1 ) % snakeSize;
+	
+		if( this.body[this.headIndex].x < 0 || 
+		    this.body[this.headIndex].x > canvasWidth ||
+		    this.body[this.headIndex].y < 0 ||
+		    this.body[this.headIndex].y > canvasHeight ){
+			this.reset();
+		}
+	}
+
+
+	this.setDirection = function( label ){
+		this.direction.set( label );
+	}
+}
 
 function init()
 {
 	stage = new createjs.Stage( "snakeCanvas" );
 
-	for( i = 0; i < snakeSize; i++ ){
-		snake[i] = new createjs.Shape();
-		snake[i].graphics.beginFill("black");
-		snake[i].graphics.drawRect( 0, 0, CELL_SIZE, CELL_SIZE );
-		stage.addChild( snake[i] );
-	}
-	resetSnake();
+	snake = new Snake();
+	snake.reset();
 	
 	canvasWidth = document.getElementById( "snakeCanvas" ).width;
 	canvasHeight = document.getElementById( "snakeCanvas" ).height;
@@ -29,46 +64,22 @@ function init()
 }
 
 
-function resetSnake()
-{
-	snakeTailIndex = 0;
-	snakeHeadIndex = snakeSize - 1;
-	for( i = 0; i < snakeSize; i++ ){
-		snake[i].x = i * CELL_SIZE;
-		snake[i].y = 0;
-		direction.set( 'right' );
-	}
-}
-
-
 function update()
 {
-	snake[snakeTailIndex].x = snake[snakeHeadIndex].x + direction.x * CELL_SIZE;
-	snake[snakeTailIndex].y = snake[snakeHeadIndex].y + direction.y * CELL_SIZE;
-
-	snakeHeadIndex = ( snakeHeadIndex + 1 ) % snakeSize;
-	snakeTailIndex = ( snakeTailIndex + 1 ) % snakeSize;
-	
-	if( snake[snakeHeadIndex].x < 0 || 
-	    snake[snakeHeadIndex].x > canvasWidth ||
-	    snake[snakeHeadIndex].y < 0 ||
-	    snake[snakeHeadIndex].y > canvasHeight ){
-		resetSnake();
-	}
-
+	snake.update( stage );
 	stage.update();
 }
 
 
 function handleKeyDown( event )
 {
-	if( event.keyCode == 37 && direction.label != 'right' ){
-		direction.set( 'left' );
-	}else if( event.keyCode == 39 && direction.label != 'left' ){
-		direction.set( 'right' );
-	}else if( event.keyCode == 38 && direction.label != 'down' ){
-		direction.set( 'up' );
-	}else if( event.keyCode == 40 && direction.label != 'up' ){
-		direction.set( 'down' );
+	if( event.keyCode == 37 && snake.direction.label != 'right' ){
+		snake.setDirection( 'left' );
+	}else if( event.keyCode == 39 && snake.direction.label != 'left' ){
+		snake.setDirection( 'right' );
+	}else if( event.keyCode == 38 && snake.direction.label != 'down' ){
+		snake.setDirection( 'up' );
+	}else if( event.keyCode == 40 && snake.direction.label != 'up' ){
+		snake.setDirection( 'down' );
 	}
 }
