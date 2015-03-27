5f6fec01f907ba73f9c8e83f238377a8eab13b1c
moisesjbc <moisesjbc@gmail.com>
2015-03-27 11:34:22 +0000

    Added snake's fixed movement.

--- snake.js
+++ snake.js
@@ -1,15 +1,25 @@
 var stage;
 var CELL_SIZE = 20;
+var snakeHead = ;
 
 function init()
 {
 	stage = new createjs.Stage( "snakeCanvas" );
 
-	var snakeHead = new createjs.Shape();
-	snakeHead.graphics.beginFill("black").drawRect( 0, 0, CELL_SIZE, CELL_SIZE );
+	snakeHead = new createjs.Shape();
+	snakeHead.graphics.beginFill("black");
+	snakeHead.drawRect( 0, 0, CELL_SIZE, CELL_SIZE );
 	snakeHead.x = 0;
 	snakeHead.y = 0;
 	stage.addChild( snakeHead );
 
+	createjs.Ticker.setFPS( 5 );
+	createjs.Ticker.addEventListener( "tick", update );
+}
+
+
+function update()
+{
+	snakeHead.x += CELL_SIZE;
 	stage.update();
 }
