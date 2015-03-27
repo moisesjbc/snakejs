78fa6fa5f0861a09f4f926c59b36e423b5bd154f
moisesjbc <moisesjbc@gmail.com>
2015-03-27 13:11:01 +0000

    Class Direction has been moved to its own file "js/direction.js".

--- js/snake.js
+++ js/snake.js
@@ -4,37 +4,6 @@ var snakeHead;
 var direction = new Direction( 'right' );
 var canvasWidth, canvasHeight;
 
-function Direction( label )
-{
-	this.set = function( label )
-	{
-		switch( label ){
-			case 'right':
-				this.label = 'right';
-				this.x = 1;
-				this.y = 0;
-			break;
-			case 'left':
-				this.label = 'left';
-				this.x = -1;
-				this.y = 0;
-			break;
-			case 'up':
-				this.label = 'up';
-				this.x = 0;
-				this.y = -1;
-			break;
-			case 'down':
-				this.label = 'down';
-				this.x = 0;
-				this.y = 1;
-			break;
-		}
-	}
-	this.set( label );
-}
-
-
 function init()
 {
 	stage = new createjs.Stage( "snakeCanvas" );
