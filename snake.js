2581efa630d9650b1d5bf19ed5115b850f90b7a3
moisesjbc <moisesjbc@gmail.com>
2015-03-27 12:04:10 +0000

    Added snake movement.

--- snake.js
+++ snake.js
@@ -1,7 +1,7 @@
 var stage;
 var CELL_SIZE = 20;
 var snakeHead;
-var direction = { x:1, y:0 };
+var direction = { label:'right', x:1, y:0 };
 
 function init()
 {
@@ -16,6 +16,8 @@ function init()
 
 	createjs.Ticker.setFPS( 5 );
 	createjs.Ticker.addEventListener( "tick", update );
+
+	document.addEventListener( "keydown", handleKeyDown );
 }
 
 
@@ -24,4 +26,26 @@ function update()
 	snakeHead.x += direction.x * CELL_SIZE;
 	snakeHead.y += direction.y * CELL_SIZE;
 	stage.update();
+}
+
+
+function handleKeyDown( event )
+{
+	if( event.keyCode == 37 && direction.label != 'right' ){
+		direction.label = 'left';
+		direction.x = -1;
+		direction.y = 0;
+	}else if( event.keyCode == 39 && direction.label != 'left' ){
+		direction.label = 'right';
+		direction.x = 1;
+		direction.y = 0;
+	}else if( event.keyCode == 38 && direction.label != 'down' ){
+		direction.label = 'up';
+		direction.x = 0;
+		direction.y = -1;
+	}else if( event.keyCode == 40 && direction.label != 'up' ){
+		direction.label = 'down';
+		direction.x = 0;
+		direction.y = 1;
+	}
 }
